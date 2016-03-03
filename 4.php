4f0fe6045bf21f7ca60ad363e676f3b6e6e1b080
huangby19890920@gmail.com <huangby19890920@gmail.com>
2016-03-03 15:40:01 +0800

    modified 4.php

--- 4.php
+++ 4.php
@@ -7,7 +7,7 @@ use PhpAmqpLib\Message\AMQPMessage;
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
-$channel->queue_declare('task_queue', false, true, false, false);
+
 echo "这是我在2016年3月3日14:41:37加的一行";
 $data = implode(' ', array_slice($argv, 1));
 if(empty($data)) $data = "Hello World!";
@@ -15,6 +15,13 @@ echo "1111111111111";
 $msg = new AMQPMessage($data,
                         array('delivery_mode' => 2) # make message persistent
                       );
+
+if ($params['AdSearch']['product'] != '') {
+        $query->andWhere(['like', 'nao.product', $params['AdSearch']['product']]);
+    }
+    if ($params['AdSearch']['partners'] != '') {
+        $query->andWhere(['like', 'nao.partners', $params['AdSearch']['partners']]);
+    }
 
 $channel->basic_publish($msg, '', 'task_queue');
 
